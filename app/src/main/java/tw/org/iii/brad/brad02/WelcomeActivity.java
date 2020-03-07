8d5db1b635ae10c56a191978e3d3451b632324c6
user <brad@brad.tw>
2020-03-07 15:05:08 +0800

    07

--- app/src/main/java/tw/org/iii/brad/brad02/WelcomeActivity.java
+++ app/src/main/java/tw/org/iii/brad/brad02/WelcomeActivity.java
@@ -2,13 +2,32 @@ package tw.org.iii.brad.brad02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        view = findViewById(R.id.welcome);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoMain();
+            }
+        });
     }
+
+    private void gotoMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
